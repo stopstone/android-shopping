6946b4e6cc1da2d0d5ff376504a6374d3236442a
StopStone <wltjr6432@naver.com>
2024-02-14 00:56:18 +0900

    fix: 선택된 navigation에 따른 Vector image변경되지 않는 버그 수정
    - itemIconTintList = null

--- app/src/main/java/com/stopstone/shoppingapp/HomeActivity.kt
+++ app/src/main/java/com/stopstone/shoppingapp/HomeActivity.kt
@@ -2,10 +2,14 @@ package com.stopstone.shoppingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val bottomNavigationHome = findViewById<BottomNavigationView>(R.id.bottom_navigation_home)
+        bottomNavigationHome.itemIconTintList = null
     }
 }
