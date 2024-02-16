3121265edc638dcf37c8562ab2485dd9a8b79155
stopstone <wltjr6432@naver.com>
2024-02-16 15:18:56 +0900

    design: viewpager page 위치 변경

--- app/src/main/java/com/stopstone/shoppingapp/HomeFragment.kt
+++ app/src/main/java/com/stopstone/shoppingapp/HomeFragment.kt
@@ -13,6 +13,8 @@ class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
     private lateinit var assetLoader: AssetLoader
+    private val bannerAdapter = HomeBannerAdapter()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,16 +36,30 @@ class HomeFragment : Fragment() {
     }
 
     private fun setLayout() {
-        val adapter = HomeBannerAdapter()
-        binding.viewpagerHomeBanner.adapter = adapter
-
+        setBanners()
         val result = assetLoader.loadAsset("home.json")
         if (!result.isNullOrEmpty()) {
             val moshi = Moshi.Builder().build()
             val jsonAdapter = moshi.adapter(BannersJsonData::class.java)
             jsonAdapter.fromJson(result)?.let {
-                adapter.add(it.banners)
+                bannerAdapter.add(it.banners)
             }
+        }
+    }
+
+    private fun setBanners() {
+        with(binding.viewpagerHomeBanner) {
+            adapter = bannerAdapter
+
+            val screenWidth = resources.displayMetrics.widthPixels
+            val pageWidth = resources.getDimension(R.dimen.viewpager_item_width)
+            val pageMargin = resources.getDimension(R.dimen.viewpager_item_margin)
+
+            val offset = screenWidth - pageWidth - pageMargin
+            setPageTransformer { page, position ->
+                page.translationX = position * -offset
+            }
+            offscreenPageLimit = 2
         }
     }
 
